dffbdf3ad910b8464f86e062864990567a3ac86c
Pavel Cherepanov <cherepanov.pavel@list.ru>
2023-02-06 13:25:06 +0500

    Change Models.py

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -20,7 +20,7 @@ class Category(models.Model):
     category = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
-        return self.name.title()
+        return self.category.title()
 
 
 class Post(models.Model):
